cce6c8e78d29a049e88f40641f7df1e290d896ef
Kylo-bytebit <131902634+Kylo-bytebit@users.noreply.github.com>
2024-07-19 23:32:28 -0400

    added past projects

---
+++ HTML ,CSS  and PHP Projects/Major project/V1.1/main.js
@@ -0,0 +1,19 @@
+var slider = document.getElementById("myrange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function(){
+  output.innerHTML = this.value;
+}
+// slideshow
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+}, 3000);
